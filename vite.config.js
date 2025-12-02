3456c5cfc6430a2e29b9c704aed3257b4ad19a2a
Arpita2265 <arpita.t144@gmail.com>
2025-12-02 13:34:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(), ],
-  base: "/Assignment5-React-",
+  base: "/Assignment5",
 })
